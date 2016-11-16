cb154fc6605433534ee540ecbd4afb8897bdaf1c
lxy <lxy@localhost.localdomain>
2016-11-16 15:42:45 +0800

    Initial commit

---
+++ mylibrary/src/test/java/commonlibrary/listener/DisposeDataListener.java
@@ -0,0 +1,11 @@
+package commonlibrary.listener;
+
+/**
+ * Created by lxy on 16-11-14.
+ */
+
+public interface DisposeDataListener {
+
+    public void onSuccess(Object responseObj);
+    public void onFailure(Object reasonObj);
+}
---
+++ mylibrary/src/test/java/commonlibrary/request/RequestParams.java
@@ -0,0 +1,29 @@
+package commonlibrary.request;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * Created by lxy on 16-11-14.
+ */
+
+public class RequestParams {
+    public ConcurrentHashMap<String,String> urlParams =new ConcurrentHashMap<>();
+    //public ConcurrentHashMap<String,Object> fileParams =new ConcurrentHashMap<>();
+
+
+    public RequestParams(Map<String,String> source)
+    {
+        if (source!=null)
+        {
+            for (Map.Entry<String,String> entry:source.entrySet())
+            {
+                put(entry.getKey(),entry.getValue());
+            }
+        }
+    }
+    void put(String key,String value)
+    {
+        urlParams.put(key,value);
+    }
+}
